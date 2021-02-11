29b8d7896a6208bdfa7ccc23f70f7d352ff1c6bc
Touhidul Shawan <touhidulshawan@gmail.com>
2021-02-11 17:17:50 +0600

    [filesystem]

--- filesystem.php
+++ filesystem.php
@@ -29,3 +29,16 @@ echo file_get_contents('./demo.txt');
 
 // put content into a file
 file_put_contents('Demo2.txt', 'This is some demo contents');
+
+// get content from url
+echo '<br/>';
+
+$data = file_get_contents('https://jsonplaceholder.typicode.com/todos');
+
+// echo $data;
+
+$todo = json_decode($data, true);
+// true specify that we want to convert it into associative array
+echo '<pre>';
+var_dump($todo);
+echo '</pre>';
