ee596d14a29467e3a232d382393544b47e7b780b
Touhidul Shawan <touhidulshawan@gmail.com>
2021-02-11 15:09:29 +0600

    associative array

--- array.php
+++ array.php
@@ -68,3 +68,83 @@ $mergedArray = [...$fruits, ...$cars];
 echo "<pre>";
 var_dump($mergedArray);
 echo "</pre>";
+
+// split string into array
+$food = "Burger, Pizza, Steak, Rice";
+
+$foodArray = explode(",", $food);
+
+// print whole things
+echo "<pre>";
+var_dump($foodArray);
+echo "</pre>";
+
+// convert array to string
+$foodType = implode(",", $foodArray);
+
+echo var_dump($foodType);
+
+
+// check element does exist on array
+echo "<pre>";
+var_dump(in_array("Mango", $fruits));
+echo "</pre>";
+
+// search element index in array
+echo "<pre>";
+var_dump(array_search("Mango", $fruits)); // return index 1
+echo "</pre>";
+
+// sorting array
+sort($fruits);
+echo "<pre>";
+var_dump($fruits);
+echo "</pre>";
+
+// for reverse sort we use rsort()
+// sorting array in reverse way
+rsort($fruits);
+echo "<pre>";
+var_dump($fruits);
+echo "</pre>";
+
+// associative array
+// associative array is key pair value array
+$persons = [
+    "name" => "Touhidul Islam",
+    "surname" => "Shawan",
+    "age" => 25, "Learning" => ["PHP", "Laravel"]
+];
+
+echo "<pre>";
+var_dump($persons);
+echo "</pre>";
+
+// get element by key
+echo $persons["name"] . "<br/>";
+
+// set element by key
+$persons["food"] = "Chocolate";
+
+echo "<pre>";
+var_dump($persons);
+echo "</pre>";
+
+// null coalescing assignment operator
+
+$persons["address"] ??= "unknown";
+
+echo "<pre>";
+var_dump($persons);
+echo "</pre>";
+
+// print the keys of array
+echo "<pre>";
+var_dump(array_keys($persons));
+echo "</pre>";
+
+// print values of array
+
+echo "<pre>";
+var_dump(array_values($persons));
+echo "</pre>";
